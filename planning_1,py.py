aac61e8ff7cb7ef1c62333353b88879f46a7875c
Swopnab <89373272+Swopnab@users.noreply.github.com>
2021-09-05 15:24:43 +0545

    Add files via upload

---
+++ planning_1,py.py
@@ -0,0 +1,16 @@
+"""
+
+if player and computer have rock rock -------->tie
+if player and computer have paper paper -------->tie
+if player and computer have scissor scissor -------->tie
+
+if player has rock and computer has scissor --------> player win
+if player has paper and computer has rock --------> player win
+if player has scissor and computer has paper --------> player win
+
+if player has rock and computer has paper --------> player lose
+if player has paper and computer has scissor --------> player lose
+if player has scissor and computer has rock --------> player lose
+
+
+"""
